6ad9865f3215baba2afa453ce1f6b41a2d3192ad
Smith Scarborough <smithscarborough@gmail.com>
2021-05-14 13:17:25 -0500

    change port to 8000 to troubleshoot Heroku

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/lyrics', async (req, res) => {
     res.json({ lyrics })
 })
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, function() {
     var host = server.address().address;
